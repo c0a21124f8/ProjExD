c268e4e28ff1b7af53bb5581925e90fb4e0f2a01
mochizuki <c0a21124f8@edu.teu.ac.jp>
2022-06-21 17:54:07 +0900

    改良したよ #7

--- ex02/calc.py
+++ ex02/calc.py
@@ -48,21 +48,17 @@ if __name__ == "__main__":
     entry.grid(row = 0,column = 0,columnspan = 4)
 
     
-
+    word_box = ["π","sin","cos","tan"]
+    m = 1
     for i in range(1,5):        #列指定(テキスト入力欄があるので１段下げる)
         for j in range(0,4):    #行指定
             if i == 4 and j == 1 :    #＋ボタンの位置になったとき実行
                 a = "+"
             elif i == 4 and j == 2:    #＝ボタンの位置になったとき実行
                 a = "="
-            elif i == 1 and j == 3:
-                a = "π"
-            elif i == 2 and j == 3:
-                a = "sin"
-            elif i == 3 and j == 3:
-                a = "cos"
-            elif i == 4 and j == 3:
-                a = "tan"
+            elif i == m and j == 3:
+                a = word_box[m-1]
+                m += 1
             else:
                 a = 9 - (3 * (i-1)) - j     #数字計算
             button = tk.Button(root,text = f"{a}",width = 4, height=2,font = ("Times New Roman", 30))
