44bc17c5a00ac97f9c9308dcc1ee048197da3e87
mochizuki <c0a21124f8@edu.teu.ac.jp>
2022-06-21 16:45:17 +0900

    電卓プロトタイプ

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,36 +1,73 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import math
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
-    root.geometry("300x600")
 
     def button_click(event):
         btn = event.widget
         txt = btn["text"]
         #tkm.showinfo(txt,f"[{txt}]ボタンがクリックされました")
-        entry.insert(tk.END, txt)
+        if txt == "=":
+            ans = entry.get()
+            ans01 = eval(ans)
+            entry.delete(0,tk.END)
+            entry.insert(tk.END, ans01)
+        elif txt == "sin":
+            ans = entry.get()
+            ans.replace(txt,"")
+            entry.delete(0,tk.END)
+            ans01 = math.sin(int(ans))
+            entry.insert(tk.END, ans01)
+        elif txt == "cos":
+            ans = entry.get()
+            ans.replace(txt,"")
+            entry.delete(0,tk.END)
+            ans01 = math.cos(int(ans))
+            entry.insert(tk.END, ans01)
+        elif txt == "tan":
+            ans = entry.get()
+            ans.replace(txt,"")
+            entry.delete(0,tk.END)
+            ans01 = math.tan(int(ans))
+            entry.insert(tk.END, ans01)
+        elif txt == "π":
+            ans = entry.get()
+            ans.replace(txt,"")
+            entry.delete(0,tk.END)
+            ans01 = int(ans)*3.141592653
+            entry.insert(tk.END, ans01)
+        else:
+            entry.insert(tk.END, txt)
+        
 
-    entry = tk.Entry(root,justify = "right",width = 10,font = ("Times New Roman",40))
-    entry.grid(row = 0,column = 0,columnspan = 3)
 
+    entry = tk.Entry(root,justify = "right",width = 15,font = ("Times New Roman",40))
+    entry.grid(row = 0,column = 0,columnspan = 4)
 
-    for i in range(1,5):
-        for j in range(0,3):
-            if (i == 4 and j == 1) :
-                button = tk.Button(root,text = "+",width = 4, height=2,font = ("Times New Roman", 30))
-                button.grid(row = i,column = j)
-                button.bind("<1>", button_click)
-            elif(i == 4 and j == 2):
-                button = tk.Button(root,text = "=",width = 4, height=2,font = ("Times New Roman", 30))
-                button.grid(row = i,column = j)
-                button.bind("<1>", button_click)
+    
+
+    for i in range(1,5):        #列指定(テキスト入力欄があるので１段下げる)
+        for j in range(0,4):    #行指定
+            if i == 4 and j == 1 :    #＋ボタンの位置になったとき実行
+                a = "+"
+            elif i == 4 and j == 2:    #＝ボタンの位置になったとき実行
+                a = "="
+            elif i == 1 and j == 3:
+                a = "π"
+            elif i == 2 and j == 3:
+                a = "sin"
+            elif i == 3 and j == 3:
+                a = "cos"
+            elif i == 4 and j == 3:
+                a = "tan"
             else:
-                a = 9 - (3 * (i-1)) - j
-                button = tk.Button(root,text = f"{a}",width = 4, height=2,font = ("Times New Roman", 30))
-                button.grid(row = i,column = j)
-                button.bind("<1>", button_click)
+                a = 9 - (3 * (i-1)) - j     #数字計算
+            button = tk.Button(root,text = f"{a}",width = 4, height=2,font = ("Times New Roman", 30))
+            button.grid(row = i,column = j)
+            button.bind("<1>", button_click)
     
 
     root.mainloop()
